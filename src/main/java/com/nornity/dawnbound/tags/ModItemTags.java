f8eba62029282792ae3b55150f38d36ba2b18b55
SkuldNorniern <skuldnorniern@gmail.com>
2026-07-01 12:20:17 +0900

    feat: add primitive recipe tags

--- src/main/java/com/nornity/dawnbound/tags/ModItemTags.java
+++ src/main/java/com/nornity/dawnbound/tags/ModItemTags.java
@@ -6,6 +6,12 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 
 public class ModItemTags {
+    public static final TagKey<Item> PRIMITIVE_FIBERS =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "primitive_fibers"));
+    public static final TagKey<Item> SHARP_STONES =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "sharp_stones"));
+    public static final TagKey<Item> PRIMITIVE_TOOL_HEADS =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "primitive_tool_heads"));
     public static final TagKey<Item> SAWS =
         TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "saws"));
 }
