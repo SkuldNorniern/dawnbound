e541556cb458d436d0c17b40430ee56cff0bdb9c
SkuldNorniern <skuldnorniern@gmail.com>
2026-07-01 13:22:08 +0900

    feat: add reinforced crafting tags

--- src/main/java/com/nornity/dawnbound/tags/ModItemTags.java
+++ src/main/java/com/nornity/dawnbound/tags/ModItemTags.java
@@ -12,6 +12,18 @@ public class ModItemTags {
         TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "sharp_stones"));
     public static final TagKey<Item> PRIMITIVE_TOOL_HEADS =
         TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "primitive_tool_heads"));
+    public static final TagKey<Item> KNAPPED_TOOL_HEADS =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "knapped_tool_heads"));
+    public static final TagKey<Item> BOUND_TOOL_HEADS =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "bound_tool_heads"));
+    public static final TagKey<Item> RAWHIDE_MATERIALS =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "rawhide_materials"));
+    public static final TagKey<Item> RESIN_BINDERS =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "resin_binders"));
+    public static final TagKey<Item> CRUDE_HANDLES =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "crude_handles"));
+    public static final TagKey<Item> TOOL_BINDINGS =
+        TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "tool_bindings"));
     public static final TagKey<Item> SAWS =
         TagKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath("dawnbound", "saws"));
 }
